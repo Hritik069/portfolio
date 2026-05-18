96231e3a5fe54245db17813493a0e9fa8d054af3
Hritik Raj <hritikraj0111@gmail.com>
2026-05-18 11:24:55 +0530

    final redeploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Projects from './components/Projects'
 import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Contact from './components/Contact'
-
+// update
 function App() {
   return (
     <div className="min-h-screen">
